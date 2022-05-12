819f8c1ed8a31b2d93ac021b6d132d054f9c0659
Seungwon Lee <sam98528@naver.com>
2022-05-12 14:09:42 +0900

    ch01

---
+++ Chapter4/src/ch01/BookTest.java
@@ -0,0 +1,28 @@
+package ch01;
+
+class Book {
+
+	private String title;
+	private String author;
+
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+	
+	@Override
+	public String toString() {
+		return title + " " + author;
+	}
+}
+
+public class BookTest {
+
+	public static void main(String[] args) {
+		Book bookA = new Book("Hello", "Sam");
+		System.out.println(bookA);
+	}
+
+	
+
+}
