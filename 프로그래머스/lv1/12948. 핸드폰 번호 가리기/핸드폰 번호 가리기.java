361e34ddf5a722833ace3497434049c14772e720
Seungwon Lee <sam98528@naver.com>
2022-06-09 11:33:32 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 11.74 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        for(int i = 0; i < phone_number.length() - 4 ; i++){
+           answer = answer + '*';
+        }
+        for(int i = phone_number.length() - 4; i < phone_number.length(); i++){
+           answer = answer + phone_number.charAt(i);
+        }
+        return answer;
+    }
+}
