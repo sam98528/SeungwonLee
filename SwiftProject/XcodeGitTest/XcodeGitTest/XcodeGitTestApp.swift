26c16f7b3490bee9ab8bbd903bc3be4c5642e90e
Seungwon Lee <sam98528@naver.com>
2022-11-21 13:09:12 +0900

    This is a Test

---
+++ SwiftProject/XcodeGitTest/XcodeGitTest/XcodeGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitTestApp.swift
+//  XcodeGitTest
+//
+//  Created by Sam.Lee on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
